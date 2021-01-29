8872724fef0339fb2f0cdec4b9ac36e0e05a692a
rohhakdong <rohhakdong@shopion.com>
2021-01-29 17:10:02 +0900

    Application, ApplicationTests 추가

---
+++ src/main/java/kr/co/shopwizard/Application.java
@@ -0,0 +1,9 @@
+package kr.co.shopwizard;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+}
---
+++ src/test/java/kr/co/shopwizard/ApplicationTests.java
@@ -0,0 +1,12 @@
+package kr.co.shopwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads...");
+    }
+}
